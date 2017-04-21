9efc7454bf5eec446895127035f079f2aa73ef9f
ARSYSLAN\vmlazaro <vmlazaro@hiberus.com>
2017-04-21 14:46:54 +0200

    First state

---
+++ www/app/common/services/authServices.js
@@ -0,0 +1,37 @@
+hackApp.service( 'AuthService', [ '$q', 'ApiService', function ( $q, ApiService ) {
+    var me   = this;
+    me.token = undefined;
+
+    var isLogged = function ( ) {
+        return angular.isDefined( token );
+    };
+
+    var getToken = function ( ) {
+        return me.token;
+    };
+
+    var logout = function ( ) {
+        token = undefined;
+    };
+
+    var login = function ( userApiToken ) {
+        var loginDeferred = $q.defer();
+        ApiService.login( { token : userApiToken } ).$promise
+        .then( function ( response ) {
+            me.token = userApiToken;
+            loginDeferred.resolve( true );
+        } )
+        .catch( function ( error ) {
+            me.token = undefined;
+            loginDeferred.reject( 'Token invalid.' );
+        } );
+
+        return loginDeferred.promise;
+    };
+
+    return {
+        isLogged : isLogged,
+        logout   : logout,
+        login    : login
+    }
+} ] );
